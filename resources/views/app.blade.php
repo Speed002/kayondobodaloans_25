0636c2345d83f0278863b19650edc28598ac857b
Speed Wasswa <speedwasswa02@gmail.com>
2025-03-07 12:06:43 +0300

    Added css and js links just incase build files refuse to run

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,10 @@
     @inertiaHead
     @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- <script src="https://cdn.tailwindcss.com"></script>
+    <link href="{{ asset('build/assets/app-a20cebfd.css') }}" rel="stylesheet">
+    <link href="{{ asset('build/assets/app-a546d813.css') }}" rel="stylesheet">
+    <script src="{{ asset('build/assets/app-b8a140d8.js') }}" type="module"></script> --}}
 </head>
 <body>
     @inertia
